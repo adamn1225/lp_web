18df4cf283fcc41d9a8427f7298433079d60c2fc
anoah1225@gmail.com <anoah1225@gmail.com>
2024-07-29 14:03:14 -0400

    cmon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,17 @@ import alpinejs from '@astrojs/alpinejs';
 import icon from 'astro-icon';
 import vercel from '@astrojs/vercel/static';
 import react from '@astrojs/react';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
   vite: {
     // Add any Vite-specific configuration here if needed
   },
-  output: 'static',
+  output: 'hybrid',
   adapter: vercel(),
-  integrations: [
-    tailwind(),
-    icon(),
-    alpinejs(),
-    react(),
-  ],
+  integrations: [tailwind(), icon(), alpinejs(), react()]
 });
