e6f8f0961032590e880c65c9ecaf070f989df3d5
anoah1225@gmail.com <anoah1225@gmail.com>
2024-07-29 14:12:26 -0400

    fda

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import alpinejs from '@astrojs/alpinejs';
-import icon from 'astro-icon';
-import vercel from '@astrojs/vercel/static';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
+import icon from "astro-icon";
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
-import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
-  build: {
-    outDir: 'dist'
-  },
   vite: {
     // Add any Vite-specific configuration here if needed
   },
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), icon(), alpinejs(), react()]
+  integrations: [
+    tailwind(),
+    icon(),
+    alpinejs(),
+    react(),
+  ],
 });
