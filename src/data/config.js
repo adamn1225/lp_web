ab6f0dddb396fdf1a196bb7d23fd07a11ed71219
anoah1225@gmail.com <anoah1225@gmail.com>
2024-07-30 21:58:41 -0400

    last try.. maybe

--- src/data/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP = void 0;
-exports.APP = {
-    name: "Line Properties",
-    description: "something",
-};
