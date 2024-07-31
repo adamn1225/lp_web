d5f033a227c4e182fe1b1effa2edb67c7ad792ef
anoah1225@gmail.com <anoah1225@gmail.com>
2024-07-30 23:12:57 -0400

    fsdasd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 import react from '@astrojs/react';
 
 export default defineConfig({
   vite: {
     // Add any Vite-specific configuration here if needed
   },
-  output: 'server',
-  adapter: vercel({
-    isr: true,
-  }),
+  output: 'static',
+  adapter: vercel(),
   integrations: [
     tailwind(),
     icon(),
