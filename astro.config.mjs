477e3f7fb648785bd22b7d950b03cef6c94e470e
anoah1225@gmail.com <anoah1225@gmail.com>
2024-07-31 12:14:24 -0400

    actsa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
-import vercel from '@astrojs/vercel/static';
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 
 export default defineConfig({
   vite: {
     // Add any Vite-specific configuration here if needed
   },
-  output: 'static',
-  adapter: vercel(),
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     tailwind(),
     icon(),
