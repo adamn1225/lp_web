1a7ff1bc0b64e0f2bc148bf481eaf499ba5489b8
anoah1225@gmail.com <anoah1225@gmail.com>
2024-08-01 23:27:58 -0400

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  vite: {
-  },
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     tailwind(),
     icon(),
