cbec3286d4847532630126256a005e4a2a0c81f3
anoah1225@gmail.com <anoah1225@gmail.com>
2024-08-02 21:13:09 -0400

    sdfasd

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -22,6 +22,8 @@ export interface BlogPost {
 
 export const contentfulClient = createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.CONTENTFUL_ACCESS_TOKEN,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
