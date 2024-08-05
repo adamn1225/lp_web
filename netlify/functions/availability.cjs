8bbcdae109f089e7bfd25b4d454f886e22a1f960
anoah1225@gmail.com <anoah1225@gmail.com>
2024-08-05 19:53:33 -0400

    function update

--- netlify/functions/availability.cjs
+++ netlify/functions/availability.cjs
@@ -1,6 +1,6 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
-export const handler = async (event, context) => {
+exports.handler = async (event, context) => {
   console.log('Received event:', event);
 
   const { checkIn, checkOut, minOccupancy } = event.queryStringParameters;
