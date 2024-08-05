bdb131909234add8a3b03ba8af0ec0d0b53a358c
anoah1225@gmail.com <anoah1225@gmail.com>
2024-08-04 21:04:19 -0400

    lets go

--- src/components/AvailabilitySearch.tsx
+++ src/components/AvailabilitySearch.tsx
@@ -94,7 +94,7 @@ const AvailabilitySearch: React.FC = () => {
   };
 
   return (
-    <div className="bg-[url('/images/line-hero.png')] bg-slate-700 pt-64 md:pt-96 bg-contain md:bg-left bg-bottom bg-no-repeat w-full h-full">
+    <div className="bg-[url('/images/line-hero.png')] bg-slate-700 pt-64 md:pt-96 bg-auto static md:bg-left bg-center bg-repeat-y w-full h-full">
 <div className="w-full items-end align-bottom md:pb-12 pb-40">
     <div className="flex flex-row justify-center w-full">
         <button onClick={toggleModal} className="flex align-bottom justify-center h-full bg-cyan-600 md:mb-28 md:w-1/5 sm:w-4/5 py-3 px-2 shadow-md shadow-cyan-500/30 rounded-xl text-white">
@@ -103,28 +103,28 @@ const AvailabilitySearch: React.FC = () => {
     </div>
     {isModalOpen && (
   <div className="fixed inset-0 bg-black bg-opacity-50 object-contain flex align-evenly justify-evenly items-center overflow-auto">
-    <div className="bg-slate-100 md:p-6 p-4 rounded-lg shadow-lg md:flex justify-center md:w-3/5 sm:w-4/5 w-full overflow-hidden">
+    <div className="bg-slate-100 pb-10 mt-12 mx-1 lg:p-6 rounded-lg shadow-lg md:flex justify-center lg:w-1/5 sm:w-4/5 w-full overflow-hidden">
 
       <form onSubmit={handleSubmit} className="w-full">
-        <div className="flex flex-col gap-3 items-center justify-center w-full">
-          <button onClick={toggleModal} className="bg-red-500 mb-2 sm:my-12 flex justify-center md:w-2/3 w-full z-50 font-bold text-muted-50 text-xl rounded-md py-3">
+        <div className="flex flex-col gap-3 items-center xs:mx-2 justify-center md:w-full">
+          <button onClick={toggleModal} className="bg-red-500 mb-2 flex xs:mt-6 justify-center md:w-3/5 w-4/5 z-50 font-bold text-muted-50 text-xl rounded-md py-3">
             Close Property Search
           </button>
-          <div className="flex flex-col md:flex-row md:w-1/4 justify-center items-center w-full">
-            <div className="px-2 pb-2 pt-3 bg-slate-100 drop-shadow border rounded-lg w-full overflow-hidden">
+          <div className="flex flex-col md:flex-row sx:w-full justify-center items-center">
+            <div className="px-2 pb-2 pt-3 w-full bg-slate-100 drop-shadow border rounded-lg md:w-full xs:w-full overflow-hidden">
               <h3 className="text-center pb-4 text-2xl">How long is your trip?</h3>
               <DateRange
                 editableDateInputs={true}
                 onChange={item => setState([item.selection])}
                 moveRangeOnFirstSelection={false}
                 ranges={state}
-                className="w-full"
+                className="max-w-max"
               />
             </div>
           </div>
 
           <div className="w-full flex align-middle justify-center h-full">
-            <button type="submit" className="flex align-middle justify-center h-full bg-cyan-600 m-0 md:w-2/3 w-full py-3 sm:mx-12 font-bold text-xl rounded-md text-muted-50">
+            <button type="submit" className="flex align-middle justify-center h-full bg-cyan-600 m-0 md:w-3/5 w-full py-3 px-1 xs:mx-2 font-bold text-xl rounded-md text-muted-50">
               <Search size={24} /> <h3>Search available properties</h3>
             </button>
           </div>
@@ -164,7 +164,7 @@ const AvailabilitySearch: React.FC = () => {
                     <img className="w-full h-64" src={property.picture.thumbnail} alt={property.picture.caption} />
                    <div className="md:p-5 p-2 bg-white flex flex-col align-middle h-1/1 justify-center border-t-2 border-slate-500/30">
                     <h3 className="font-sans font-bold text-lg text-muted-900 pb-4 text-center">{property.title}</h3>
-                   <div className="flex md:flex-row flex-col justify-center self-center md:justify-between border-t-2 border-slate-500/30 pt-6"> 
+                   <div className="flex flex-col mb-2 justify-center self-center md:justify-between border-t-2 border-slate-500/30 pt-6"> 
                     <p className="font-sans font-bold text-lg text-center text-muted-900 ">Price: ${property.prices.basePrice} {property.prices.currency}</p>
                     <a href={property._id}><button className=" bg-cyan-600 m-0 py-3 md:px-12 px-4 shadow-md shadow-cyan-500/30 rounded-xl text-white">Book Instantly!</button></a> 
                     </div>
