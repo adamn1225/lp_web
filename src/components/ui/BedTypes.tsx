57b86330642525928f46fda28561ff3d1717c1b4
adamn1225 <anoah1225@gmail.com>
2025-01-17 14:00:24 -0500

    remove bed types title if no bedtypes, ui fixes, removed unneeded content

--- src/components/ui/BedTypes.tsx
+++ src/components/ui/BedTypes.tsx
@@ -20,6 +20,8 @@ const getBedIcon = (type: string) => {
     switch (type) {
         case 'SINGLE_BED':
             return <IoBedOutline className="w-5 h-5" />;
+        case 'SOFA_BED':
+            return <BedDoubleIcon className="w-5 h-5" />;
         case 'DOUBLE_BED':
             return <BedDouble className="w-5 h-5" />;
         case 'QUEEN_BED':
@@ -32,9 +34,11 @@ const getBedIcon = (type: string) => {
 };
 
 const BedTypes: React.FC<BedTypesProps> = ({ beds }) => {
+    const hasBeds = beds.some(room => room.beds.length > 0);
+
     return (
         <div className="my-4">
-            <h3 className="text-xl font-bold mb-2 text-center">Bed Types</h3>
+            {hasBeds && <h3 className="text-xl font-bold mb-2 text-center">Bed Types</h3>}
             <div className="flex flex-wrap gap-4">
                 {beds.filter((room) => room.beds.length > 0).flatMap((room) => (
                     room.beds.map((bed, bedIndex) => (
