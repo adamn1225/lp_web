0c6788abe0edf49dee9df0254692b392883cfa52
adamn1225 <anoah1225@gmail.com>
2025-02-02 18:45:26 -0500

    restructure blog files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,11 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import markdoc from "@astrojs/markdoc";
 import image from "@astrojs/image";
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://line-properties.com',
+  integrations: [sitemap()],
   output: 'server',
   adapter: netlify(),
   integrations: [tailwind(), alpinejs(), icon(), react(), markdoc(), image()],
