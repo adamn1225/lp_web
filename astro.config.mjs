831009b4911b4eade982a8bf148fc66ebadd5892
adamn1225 <anoah1225@gmail.com>
2025-02-02 20:46:55 -0500

    cms - reallly reallyl lsat one

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,27 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
-import icon from "astro-icon";
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
 import markdoc from "@astrojs/markdoc";
 import image from "@astrojs/image";
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpinejs';
+import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";
+import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://line-properties.com',
-  integrations: [sitemap()],
   output: 'server',
   adapter: netlify(),
-  integrations: [tailwind(), alpinejs(), icon(), react(), markdoc(), image()],
+  integrations: [
+    tailwind(),
+    alpinejs(),
+    icon(),
+    react(),
+    markdoc(),
+    image(),
+    sitemap(),
+  ],
   vite: {
     server: {
       middlewareMode: false,
