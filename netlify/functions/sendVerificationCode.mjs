c58879b7d947133abac62fc03ff014cbdede33a6
adamn1225 <anoah1225@gmail.com>
2025-02-05 12:05:44 -0500

    twilio

--- netlify/functions/sendVerificationCode.mjs
+++ netlify/functions/sendVerificationCode.mjs
@@ -1,9 +1,9 @@
 import { Twilio } from 'twilio';
 import { parsePhoneNumberFromString } from 'libphonenumber-js';
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID; 
-const authToken = process.env.TWILIO_AUTH_TOKEN; 
-const serviceSid = process.env.TWILIO_SERVICE_SID; 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceSid = process.env.TWILIO_SERVICE_SID;
 
 const client = new Twilio(accountSid, authToken);
 
