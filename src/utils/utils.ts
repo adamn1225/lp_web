b6913be34b4d9f0788ed4516c00dca822152b984
adamn1225 <anoah1225@gmail.com>
2025-02-18 16:15:04 -0500

    asdfasd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,18 +1,14 @@
-import { fetchOneHundred, fetchThreeHundred, fetchTwoHundred, fetchFeaturedListings } from '../pages/api/fetch';
+import { fetchFeaturedListings, fetchListings } from '../pages/api/fetch';
 
 async function fetchAllListings() {
-  const [oneHundred, threeHundred, twoHundred, featured] = await Promise.all([
-    fetchOneHundred(),
-    fetchThreeHundred(),
-    fetchTwoHundred(),
+  const [fetchAll, featured] = await Promise.all([
+    fetchListings(),
     fetchFeaturedListings(),
   ]);
 
   // Combine the listings into one object or array as needed
   const combinedListings = {
-    oneHundred,
-    threeHundred,
-    twoHundred,
+    fetchAll,
     featured
   };
 
@@ -23,8 +19,8 @@ const allListings = await fetchAllListings();
 
 export function processListing(title: string, _id: string) {
   return {
-    title: title === "" ? allListings.oneHundred.title : title,
-    _id: _id === "" ? allListings.oneHundred._id : _id
+    title: title === "" ? allListings.fetchAll[0]?.title : title,
+    _id: _id === "" ? allListings.fetchAll[0]?._id : _id
   };
 }
 
@@ -37,19 +33,19 @@ export async function fetchReservedDates(listingId: string, startDate: string, e
   try {
     // Get the API token from environment variables
     const apiToken = import.meta.env.VITE_API_TOKEN;
-  
+
     // Log the API token for debugging
     console.log('API Token:', apiToken);
-  
+
     // Check if the API token is set
     if (!apiToken) {
       throw new Error('API token is not set. Please check your environment variables.');
     }
-  
+
     // Construct the URL for the API request
     const url = `https://open-api.guesty.com/v1/availability-pricing/api/calendar/listings/${listingId}?startDate=${startDate}&endDate=${endDate}`;
     console.log(`Fetching reserved dates from URL: ${url}`);
-  
+
     // Make the API request
     const response = await fetch(url, {
       method: 'GET',
@@ -58,16 +54,16 @@ export async function fetchReservedDates(listingId: string, startDate: string, e
         'Authorization': `Bearer ${apiToken}`
       }
     });
-  
+
     // Get the response text
     const responseText = await response.text();
     console.log('Response Text:', responseText);
-  
+
     // Check if the response is not OK
     if (!response.ok) {
       throw new Error(`Failed to fetch reserved dates: ${response.statusText}`);
     }
-  
+
     // Parse the response JSON
     const data = JSON.parse(responseText);
     return data.dates; // Adjust this based on the actual response structure
